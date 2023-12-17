e774edda3399246df06ea01b19e73731db818c5d
Lisa <lizamotorna2004@gmail.com>
2023-12-17 16:47:17 +0200

    adaptive header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     new WOW().init();
 });
+
+const menuBtn = document.querySelector('.burger');
+const menuBody = document.querySelector('.header__links');
+
+menuBtn.addEventListener('click', () => {
+    menuBody.classList.toggle('header__links--active');
+    menuBtn.classList.toggle('burger--active');
+});
