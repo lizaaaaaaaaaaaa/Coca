365212d7e7c710a2590f9fd59eb63e129b46e73e
Lisa <lizamotorna2004@gmail.com>
2023-12-17 21:23:49 +0200

    fixed errors with wow in header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const nav = document.querySelector('.nav');
+const button = document.querySelector('.header__btn');
+
+function addWow() {
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  if (width >= 690) {
+    nav.classList.add('wow', 'fadeInRight');
+    button.classList.add('wow', 'fadeInRight');
+  } else {
+    nav.classList.remove('wow', 'fadeInRight');
+    button.classList.remove('wow', 'fadeInRight');
+  }
+}
+
+addWow();
+
 document.addEventListener('DOMContentLoaded', function () {
     new WOW().init();
 });
