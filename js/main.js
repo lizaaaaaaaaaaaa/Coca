2fdac22e8163cfaadbb54142bc81fa48dc4b0f88
Lisa <lizamotorna2004@gmail.com>
2024-02-04 21:23:05 +0200

    adding classList check

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const nav = document.querySelector(".nav");
 const button = document.querySelector(".header__btn");
+const addressTable = document.querySelector(".connection__address");
 
-var width =window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
 function addWow() {
+  // Перевірка чи елементи знайдено перед додаванням / видаленням класів
+  if (nav && button) {
+    if (width > 690) {
+      nav.classList.add("wow", "fadeInRight");
+      button.classList.add("wow", "fadeInRight");
+    } else {
+      nav.classList.remove("wow", "fadeInRight");
+      button.classList.remove("wow", "fadeInRight");
+    }
+  }
 
-  if (width > 690) {
-    nav.classList.add("wow", "fadeInRight");
-    button.classList.add("wow", "fadeInRight");
-  } else {
-    nav.classList.remove("wow", "fadeInRight");
-    button.classList.remove("wow", "fadeInRight");
+  if (addressTable) {
+    if (width > 775) {
+      addressTable.classList.remove("wow", "fadeInRight");
+    } else {
+      addressTable.classList.add("wow", "fadeInRight");
+    }
   }
 }
 
@@ -23,11 +34,13 @@ document.addEventListener("DOMContentLoaded", function () {
 const menuBtn = document.querySelector(".burger");
 const menuBody = document.querySelector(".header__links");
 
-menuBtn.addEventListener("click", () => {
-  menuBody.classList.toggle("header__links--active");
-  menuBtn.classList.toggle("burger--active");
-  menuBody.classList.add("wow", "bounceInDown");
-});
+if (menuBtn && menuBody) {
+  menuBtn.addEventListener("click", () => {
+    menuBody.classList.toggle("header__links--active");
+    menuBtn.classList.toggle("burger--active");
+    menuBody.classList.add("wow", "bounceInDown");
+  });
+}
 
 $(".insight__slider").slick({
   arrows: true,
@@ -39,32 +52,34 @@ $(".insight__slider").slick({
 $(".testim__slider").slick({
   arrows: true,
   infinite: false,
-  // appendArrows: $('.testim__arrows'),
+   // appendArrows: $('.testim__arrows'),
   // prevArrow:'<button type="button" class="testim__arrows-prev"><img src="../images/icons/active-arrow.svg"></button>',
   // nextArrow:'<button type="button" class="testim__arrows-next"><img src="../images/icons/active-arrow.svg"></button>'
 });
 
 const text = document.querySelector(".get-it__p p");
 
-text.innerHTML = text.innerText
-.split("")
-.map(
-	(char, i) => `<span style="transform:rotate(${i * 9.2}deg)">${char}</span>`
-)
-.join("");
-
-if (width > 690) {
-    $(".believe-in__slider").slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        variableWidth: true,
-    });
+if (text) {
+  text.innerHTML = text.innerText
+    .split("")
+    .map(
+      (char, i) => `<span style="transform:rotate(${i * 9.2}deg)">${char}</span>`
+    )
+    .join("");
 }
 
-$(".team__slider").slick({
+if (width > 690) {
+  $(".believe-in__slider").slick({
     arrows: false,
     autoplay: true,
     autoplaySpeed: 5000,
     variableWidth: true,
   });
+}
+
+$(".team__slider").slick({
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  variableWidth: true,
+});
